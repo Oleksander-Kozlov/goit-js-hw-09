444d3df4be9362db1069c2aaa7b900fb8dd8d396
Oleksander-Kozlov <118023905+Oleksander-Kozlov@users.noreply.github.com>
2023-04-16 19:08:54 +0000

    Deploying to gh-pages from @ Oleksander-Kozlov/goit-js-hw-09@35c66dafa186f0a5c05c79c715634a446b6bf775 🚀

--- 01-color-switcher.b9914b25.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("body button[data-start]"),o=document.querySelector("body button[data-stop]");console.dir(o),e.addEventListener("click",(function(){e.setAttribute("disabled","true"),r=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.style.backgroundColor=e}),1e3)}));let r=null;o.addEventListener("click",(function(){clearInterval(r),e.removeAttribute("disabled","true"),e.setAttribute("active","true")}));
-//# sourceMappingURL=01-color-switcher.b9914b25.js.map
