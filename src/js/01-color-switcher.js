35c66dafa186f0a5c05c79c715634a446b6bf775
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-16 22:08:28 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,23 @@
-import { getRandomHexColor } from "../helpers/getRandomHexColor";
+import { getRandomHexColor } from '../helpers/getRandomHexColor';
 
 const body = document.querySelector('body');
 const startBtn = document.querySelector('body button[data-start]');
-const stopBtn = document.querySelector('body button[data-stop]');;
-    
-    console.dir(stopBtn);
+const stopBtn = document.querySelector('body button[data-stop]');
 
-
-startBtn.addEventListener('click', onClick)
+startBtn.addEventListener('click', onClick);
 let colorTimeChange = null;
 function onClick() {
-    startBtn.setAttribute('disabled', 'true');
-   colorTimeChange = setInterval(() => {
-        const randomColor = getRandomHexColor();
-       body.style.backgroundColor = randomColor;
-       
-    },1000)
-       
-};
+  startBtn.setAttribute('disabled', 'true');
+
+  colorTimeChange = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+  }, 1000);
+}
 stopBtn.addEventListener('click', onStopClick);
 
 function onStopClick() {
-    clearInterval(colorTimeChange);
-    startBtn.removeAttribute('disabled', 'true');
-    startBtn.setAttribute('active', 'true');
+  clearInterval(colorTimeChange);
+  startBtn.removeAttribute('disabled', 'true');
+  startBtn.setAttribute('active', 'true');
 }
