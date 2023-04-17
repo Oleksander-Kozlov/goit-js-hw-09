0de48513e9b55f1f2db790971b9d1fa176f56215
Oleksander-Kozlov <118023905+Oleksander-Kozlov@users.noreply.github.com>
2023-04-17 15:10:03 +0000

    Deploying to gh-pages from @ Oleksander-Kozlov/goit-js-hw-09@978538c0ba5c6e15e3bce64eca08f1e1e0372174 🚀

---
+++ 03-promises.b3bb3686.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form"),n=e.lastElementChild;e.delay,e.step,e.amount;n.addEventListener("click",(function(e){position,delay,Math.random(),console.log(e)}))}();
+//# sourceMappingURL=03-promises.b3bb3686.js.map
