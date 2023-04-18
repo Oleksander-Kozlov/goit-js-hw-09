f16ebc7a575025dc9f7068430fa530e8ec3ca3cb
Oleksander-Kozlov <118023905+Oleksander-Kozlov@users.noreply.github.com>
2023-04-18 05:22:53 +0000

    Deploying to gh-pages from @ Oleksander-Kozlov/goit-js-hw-09@a5181585c1260b2f30cdc4020523bd41da2d2628 🚀

--- 03-promises.d7588ba7.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),o=e.lastElementChild;e.delay,e.step,e.amount;o.addEventListener("click",(function(e){position,delay,Math.random(),console.log(e)}));
-//# sourceMappingURL=03-promises.d7588ba7.js.map
