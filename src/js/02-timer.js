ea3184956f39b77c551e689a91de8101cd5a55f7
Oleksander-Kozlov <kozlov.oleksandr89@gmail.com>
2023-04-18 20:55:40 +0300

    fix 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,73 +2,53 @@ import flatpickr from 'flatpickr';
 import notiflix from 'notiflix';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
+// функція конвертації мілісекунд
+import { convertMs } from '../helpers/convertMs';
+//дістаєм HTML-елементи
 const inputDate = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('body button[data-start]');
-
 const leftDays = document.querySelector('.value[data-days]');
-
-const leftHours    = document.querySelector('.value[data-hours]'); 
-const leftMinutes   = document.querySelector('.value[data-minutes]');
+const leftHours = document.querySelector('.value[data-hours]');
+const leftMinutes = document.querySelector('.value[data-minutes]');
 const leftSeconds = document.querySelector('.value[data-seconds]');
- let dayX = 0
-
-
+// зміна для обраної дати
+let dayX = 0;
+// опції календаря
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
- 
-    onClose(selectedDates) {
-        
-        if (selectedDates[0] < new Date() || selectedDates[0] === new Date()) {
-            notiflix.Notify.failure("Please choose a date in the future",
-                {
-              width: '300px',
-              position: 'center-top',
-              closeButton: false,
-                timeout: 3000,
-              
-            });
-        
-          notiflix.Report.failure.remove( 1923);
-          startBtn.setAttribute('disabled', 'true');
-        } else {
-          startBtn.removeAttribute('disabled', 'true');
-          startBtn.setAttribute('active', 'true');
-          dayX = selectedDates[0];
-        }
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date() || selectedDates[0] === new Date()) {
+      notiflix.Notify.failure('Please choose a date in the future', {
+        position: 'center-top',
+      });
+
+      startBtn.setAttribute('disabled', 'true');
+    } else {
+      startBtn.removeAttribute('disabled', 'true');
+      startBtn.setAttribute('active', 'true');
+      dayX = selectedDates[0];
     }
-}
-flatpickr(inputDate, options); 
+  },
+};
+flatpickr(inputDate, options);
 
-
-startBtn.addEventListener('click', onClick)
+startBtn.addEventListener('click', onClick);
 function onClick() {
-    const currentData = new Date();
-    const targetDate = dayX;
-    setInterval(() => {
-        const currentDate = new Date();
-        convertMs(targetDate - currentDate), 1000;
-    });
-    function convertMs(ms) {
-        // Number of milliseconds per unit of time
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
+  const targetDate = dayX;
+  setInterval(() => {
+    const currentDate = new Date();
+    addLeadingZero(convertMs(targetDate - currentDate)), 1000;
+  });
 
-        // Remaining days
-        const days = Math.floor(ms / day);
-        //     // Remaining hours
-        const hours = Math.floor((ms % day) / hour);
-        //     // Remaining minutes
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        //     // Remaining seconds
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-        leftSeconds.textContent = seconds;
-        leftMinutes.textContent = minutes;
-        leftHours.textContent = hours;
-        leftDays.textContent = days;
-    }
+  function addLeadingZero(value) {
+    const values = Object.values(value);
+    leftSeconds.textContent = values[3].toString().padStart(2, '0');
+    leftMinutes.textContent = values[2].toString().padStart(2, '0');
+    leftHours.textContent = values[1].toString().padStart(2, '0');
+    leftDays.textContent = values[0].toString().padStart(2, '0');
+  }
 }
